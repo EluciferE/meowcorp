c0f635ee4e368cc68aada916c12989cf25e022dd
Meow <luciferevgeniy@bk.ru>
2023-04-06 22:09:27 +0500

    add meowcorp-admin startproject

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
 
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
 requirements = [
     "python-dotenv~=0.21.0",
     "sqlalchemy~=1.4.39",
@@ -9,8 +13,13 @@ requirements = [
 
 setup(
     name="meowcorp",
-    version="0.1",
+    version="0.1.1",
+    author="Jeff Hale",
     packages=find_packages(include=['meowcorp', 'meowcorp.*', 'meowcorp.conf.project_template.*']),
+    description="Add-on to the library of aiogram",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/EluciferE/meowcorp/",
     install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3.7",
